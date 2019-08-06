01eb731da4893484b5230e63d2665afd618e8166
alvaroximenesrj <alvaroximenesrj@gmail.com>
2019-08-06 01:01:11 -0300

    only commit

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using PartyInvites.Models;
+using System;
+using System.Web.Mvc;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+            
+        }
+    }
+}
